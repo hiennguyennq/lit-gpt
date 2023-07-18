5bd7b5afe91be7948192ae1c6bbe5754d3426c8f
hiennguyennq <93762976+hiennguyennq@users.noreply.github.com>
2023-07-18 09:26:34 +0700

    Update adapter.py

--- finetune/adapter.py
+++ finetune/adapter.py
@@ -36,7 +36,7 @@ micro_batch_size = 4
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
 epoch_size = 4000  # train dataset size
-num_epochs = 2
+num_epochs = 4
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
 weight_decay = 0.02
 warmup_steps = 2 * (epoch_size // micro_batch_size) // devices // gradient_accumulation_iters  # 2 epochs
